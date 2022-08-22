943e0682fa89271581dca4bab3de97ab51f9d26c
DropDeart <frkn.furkan55@gmail.com>
2022-08-22 12:14:36 +0300

    Tetris

---
+++ src/components/Stage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cell from './Cell'
+import { StyledStage } from '../style/StyleStage'
+const Stage = ({ stage }) => {
+  return (
+    <StyledStage width={stage[0].length} height={stage.length}>
+      {stage.map((row) =>
+        row.map((cell, x) => <Cell key={x} type={cell[0]} />),
+      )}
+    </StyledStage>
+  )
+}
+
+export default Stage
